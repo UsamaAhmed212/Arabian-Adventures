66d8a08aed798eea16c0a63d069413d9dc167553
SpikE <usamaahmed212aaa@gmail.com>
2025-07-31 14:29:52 +0600

    Home page Responsive Design Complete

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,23 @@ $(document).ready(function () {
 		items: 4,       // The number of items you want to see on the screen.
         responsiveClass:true,
         navText: ['', ''],
+        responsive: {
+            0: {          // from 0px up
+                items: 2,
+                margin: 6
+            },
+            768: {        // from 768px up
+                items: 3,
+                margin: 12
+            },
+            992: {        // from 992px up
+                items: 4,
+                margin: 12
+            },
+            1200: {       // from 1200px up
+                items: 4,
+                margin: 24
+            }
+        }
     });
 });
